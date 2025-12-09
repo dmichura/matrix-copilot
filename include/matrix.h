1e581e6d89ef009a03f8ed27f8a9a223e2e619ae
dmichura <dejwidcode@gmail.com>
2025-12-09 22:41:16 +0100

    Update class name

--- include/matrix.h
+++ include/matrix.h
@@ -5,34 +5,68 @@
 #include <initializer_list>
 #include <ostream>
 
-class Matrix {
+using namespace std;
+
+class matrix {
 public:
     using size_type = std::size_t;
 
-    Matrix() noexcept;
-    Matrix(size_type rows, size_type cols, double value = 0.0);
-    Matrix(std::initializer_list<std::initializer_list<double>> init);
+    matrix() noexcept;
+    matrix(size_type rows, size_type cols, double value = 0.0);
+    matrix(std::initializer_list<std::initializer_list<double>> init);
 
-    Matrix(const Matrix&) = default;
-    Matrix(Matrix&&) noexcept = default;
-    Matrix& operator=(const Matrix&) = default;
-    Matrix& operator=(Matrix&&) noexcept = default;
-    ~Matrix() = default;
+    matrix(const matrix&) = default;
+    matrix(matrix&&) noexcept = default;
+    matrix& operator=(const matrix&) = default;
+    matrix& operator=(matrix&&) noexcept = default;
+    ~matrix() = default;
 
-    size_type rows() const noexcept { return rows_; }
-    size_type cols() const noexcept { return cols_; }
-    size_type size() const noexcept { return size_; }
+    size_type get_rows() const noexcept { return rows; }
+    size_type get_cols() const noexcept { return cols; }
+    size_type size() const noexcept { return rows * cols; }
 
     // Zarządzanie pamięcią
     void alokuj(size_type n);
 
     // Dostęp do danych
-    double& operator()(size_type r, size_type c) { return data_[r * cols_ + c]; }
-    double operator()(size_type r, size_type c) const { return data_[r * cols_ + c]; }
-    double* data() noexcept { return data_.get(); }
-    const double* data() const noexcept { return data_.get(); }
+    double& operator()(size_type r, size_type c) { return data[r][c]; }
+    double operator()(size_type r, size_type c) const { return data[r][c]; }
+
+    // Operatory arytmetyczne
+    matrix& operator+(matrix& m);
+    matrix& operator*(matrix& m);
+    matrix& operator+(int a);
+    matrix& operator*(int a);
+    matrix& operator-(int a);
+
+    // Friend operatory dla int op matrix
+    friend matrix operator+(int a, matrix& m);
+    friend matrix operator*(int a, matrix& m);
+    friend matrix operator-(int a, matrix& m);
+
+    // Inkrementacja/dekrementacja
+    matrix& operator++(int);
+    matrix& operator--(int);
+
+    // Operatory przypisania
+    matrix& operator+=(int a);
+    matrix& operator-=(int a);
+    matrix& operator*=(int a);
+
+    // Operator funkcyjny
+    matrix& operator()(double value);
+
+    // Wypisywanie
+    friend ostream& operator<<(ostream& o, matrix& m);
+
+    // Operatory porównania
+    bool operator==(const matrix& m);
+    bool operator>(const matrix& m);
+    bool operator<(const matrix& m);
+
+    // Publiczne dla friend funkcji
+    int rows, cols;
+    double** data;
 
 private:
-    size_type rows_{0}, cols_{0}, size_{0};
-    std::unique_ptr<double[]> data_;
 };
